dba06f8034ffd1040ba72f8418ee9e814a17272a
wang stephen <551619855@qq.com>
2017-03-24 14:06:45 +0800

    add new code...

--- PfGpu/kernel.cu
+++ PfGpu/kernel.cu
@@ -4,118 +4,97 @@
 
 #include <stdio.h>
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+#include <iostream>
 
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
+#include "CSVReader.h"
+#include "time_stamp.h"
+
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	//Load Data
+	CSVReader rawdatafile("C:\\Users\\steve\\Documents\\Visual\ Studio\ 2015\\Projects\\PfGpu\\Debug\\5UwbData.data.csv");
+	
+	CSVReader resultdatafile("C:\\Users\\steve\\Documents\\Visual\ Studio\ 2015\\Projects\\PfGpu\\Debug\\5RealPose.csv");
+	CSVReader beacondatafile("C:\\Users\\steve\\Documents\\Visual\ Studio\ 2015\\Projects\\PfGpu\\Debug\\5beaconset.data.csv");
+	//CSVReader datafile("C:\\Users\\steve\\Documents\\Visual\ Studio\ 2015\\Projects\\PfGpu\\Debug\\5UwbData.data.csv");
+	//std::cout << "test:kkkk:" << std::endl;
+	std::cout << "raw uwb data:" << rawdatafile.GetMatrix().GetRows()<<":"
+		<< rawdatafile.GetMatrix().GetCols() << std::endl;
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	std::cout << "result data :" << resultdatafile.GetMatrix().GetRows()<<":"
+		<< resultdatafile.GetMatrix().GetCols() << std::endl;
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	std::cout << "beaconset :" << beacondatafile.GetMatrix().GetRows()<<":"
+		<< beacondatafile.GetMatrix().GetCols() << std::endl;
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	int beacon_num = rawdatafile.GetMatrix().GetCols() - 1;
+	int raw_num_times = rawdatafile.GetMatrix().GetRows();
+	int dimisional = 3;
+	double z_offset = 1.12;
 
-    return 0;
+	double *h_raw = new double[(beacon_num + 1)*(raw_num_times + 1)];
+	double *h_beaconset = new double[(beacon_num + 1)*dimisional];
+	double *h_result = new double[(dimisional)*(raw_num_times + 1)];
+
+	for(int i(0);i<raw_num_times;i++)
+	{
+		for(int j(0);j<beacon_num;++j)
+		{
+			h_raw[i, j] = *rawdatafile.GetMatrix()(i, j + 1);
+		}
+	}
+
+	for(int i(0);i<beacon_num;++i)
+	{
+		for(int j(0);j<dimisional;++j)
+		{
+			h_beaconset[i, j] = *beacondatafile.GetMatrix()(i, j);
+		}
+	}
+
+	for(int i(0);i<raw_num_times;++i)
+	{
+		for(int j(0);j<dimisional;++j)
+		{
+			h_result[i, j] = *resultdatafile.GetMatrix()(i, j);
+		}
+	}
+
+	//Save to array
+	double *d_raw;
+	double *d_beaconset;
+	cudaMalloc((void**)&d_raw, (beacon_num + 1)*(raw_num_times + 1) * sizeof(double));
+	cudaMalloc((void**)&d_beaconset, (beacon_num + 1)*(dimisional) * sizeof(double));
+
+
+	//PF
+	int particle_num = 10000;
+	double *p_state = NULL;
+	double *p_score = NULL;
+
+	cudaMalloc((void**)&p_state, particle_num*dimisional * sizeof(double));
+	cudaMalloc((void **)&p_score, particle_num * 1 * sizeof(double));
+
+
+
+	//Sample
+
+	//Evaluate
+
+	//Get result
+
+
+	//Resample
+
+	//Compute error
+
+
+	//save output.
+
+	int a(0);
+	std::cin >> a;
+	return 0;
+
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
